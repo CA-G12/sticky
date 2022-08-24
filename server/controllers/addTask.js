73426760dbb71a9136c03cff41333e300b8c535e
Bas-sheikh <basilelshakhe@gmail.com>
2022-08-24 12:36:47 +0300

    created controllers function fofunctions for deleting tasks, adding new task, and geting all geting all tasks. relates #13

--- server/controllers/addTask.js
+++ server/controllers/addTask.js
@@ -9,7 +9,20 @@ const {
 
 const addTask = (req, res) => {
   const { task, date } = req.body;
-  res.json({ task, date });
+  getDateQuery([date])
+    .then((data) => {
+      if (data.rows.length > 0) {
+        return data.rows[0];
+      }
+      return addDateQuery([date]);
+    })
+    .then((dataDate) => dataDate)
+    .then((dateId) => {
+      const { id } = dateId;
+      return addTaskQuery([task, id]);
+    })
+    .then((sara) => res.status(200).json({ message: 'task added successfuly', state: 'success' }))
+    .catch((err) => res.status(500).send({ message: 'something went wrong', state: 'fail' }));
 };
 
 module.exports = addTask;
--- server/controllers/deleteNote.js
+++ server/controllers/deleteNote.js
@@ -1,15 +1,11 @@
 /* eslint-disable no-unused-vars */
-const {
-  addTaskQuery,
-  getTasksQuery,
-  deleteTaskQuery,
-  getDateQuery,
-  addDateQuery,
-} = require('../database/queries');
+const { deleteTaskQuery } = require('../database/queries');
 
 const deleteNote = (req, res) => {
   const { id } = req.params;
-  res.send(id);
+  deleteTaskQuery([id])
+    .then((data) => res.status(200).json({ message: 'task deleted successfuly', state: 'success' }))
+    .catch((err) => res.status(500).json({ message: 'something went wrong', state: 'fail' }));
 };
 
 module.exports = deleteNote;
